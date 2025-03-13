40b93ab2592a7d47f894c876d0c35fef8f90cbcf
PeterLi <1339590363@qq.com>
2025-03-13 16:05:58 +0800

    baseline

--- recbole/model/sequential_recommender/__init__.py
+++ recbole/model/sequential_recommender/__init__.py
@@ -1,3 +1,5 @@
 from recbole.model.sequential_recommender.cl4srec import CL4SRec
 from recbole.model.sequential_recommender.duorec import DuoRec
 from recbole.model.sequential_recommender.gru4rec import GRU4Rec
+from recbole.model.sequential_recommender.sasrec import SASRec
+from recbole.model.sequential_recommender.caser import Caser
